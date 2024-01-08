3c2b3dccc485317d8deb7e04d75069c40d18fea6
HadesK2_DESK <duongnhathao001@gmail.com>
2024-01-09 01:18:32 +0700

    Update Home page, add Header and PageContent components.

--- src/app/components/PageContent.tsx
+++ src/app/components/PageContent.tsx
@@ -1,8 +1,13 @@
 import styles from '@/app/styles/PageContent.module.scss';
+import styles2 from '@/app/styles/RockerMan.module.scss';
+import BannerSVG from "@/app/components/BannerSVG";
 
 export default function PageContent() {
     return (
         <div className={styles.pageContent}>
+            <div className={styles2.svgWrapper}>
+                <BannerSVG></BannerSVG>
+            </div>
         </div>
     )
 }
