f3ddd838b4bc73f013c0b886f7e2009013004e85
HadesK2_DESK <duongnhathao001@gmail.com>
2024-01-08 22:44:59 +0700

    Add Header and PageContent components

---
+++ src/app/components/PageContent.tsx
@@ -0,0 +1,8 @@
+import styles from '@/app/styles/PageContent.module.scss';
+
+export default function PageContent() {
+    return (
+        <div className={styles.pageContent}>
+        </div>
+    )
+}
